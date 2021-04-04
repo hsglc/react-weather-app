b2433d6509c3ed86bc89318e65f8aca2673aa9c3
huseyin gulcu <huseyingulcuu@gmail.com>
2021-04-04 21:35:59 +0300

    core logic has included to project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+
+
+function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(position => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`).then(res => res.json()).then(result => {
+        setData(result)
+        console.log(result);
+      })
+    }
+
+
+    fetchData();
+    
+  }, [lat, long]);
+
+  return (
+    <div className="App"></div>
+  );
+}
+
+export default App;
